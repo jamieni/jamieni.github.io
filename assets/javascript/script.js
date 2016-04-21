d955b5a0e1ed700fd8a6ccc67b5217ddb6554b3d
Jamie Ni <jamieni@berkeley.edu>
2016-04-20 19:02:17 -0700

    new pic

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,17 +1,3 @@
-// $(document).ready(function(){
-// 	$('.grid').masonry({
-// 	  // options
-// 	  itemSelector: '.grid-item',
-// 	  columnWidth: 200
-// 	});
-// }
-// $(document).ready(function() {
-// function getRandomSize(min, max) {
-//   return Math.round(Math.random() * (max - min) + min);
-// }
-//   for (var i = 0; i < 25; i++) {
-//     var width = getRandomSize(200, 400);
-//     var height =  getRandomSize(200, 400);
-//     $('.grid').append('<img src="http//www.lorempixel.com/'+width+'/'+height+'/cats" alt="pretty kitty">');
-//   }
-// });
+$(document).ready(function(){
+
+});
