a7b3d94ed3ef653fbd7bd1f8769f8def4697ca70
Jamie Ni <jamieni@berkeley.edu>
2016-04-20 22:44:29 -0700

    darker picture

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,29 @@
 $(document).ready(function(){
-
+	
 });
+
+
+
+
+
+
+
+
+// 1. show(), fadeIn(), or slideUp()
+// 2. hide(), fadeOut(), or slideDown()
+// 3. css()
+// 4. attr()
+// 5. addClass()
+// 6. removeClass()
+// 7. hasClass()
+// 8. click()
+// 9. hover()
+// 10. text()
+// 11. html()
+// 12. offset()
+// 13. position()
+// 14. scrollTop()
+// 15. append()
+// 16. prepend()
+// 17. children()
+// 18. parent()
