7731f8ede9f09e9bd3ffea28c830ab034a19f296
Jamie Ni <jamieni@berkeley.edu>
2016-04-26 18:11:04 -0700

    final for normal screen resolution

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,29 +1,27 @@
 $(document).ready(function(){
-	
+	$(".links").hover(function(){
+		$(this).css({"-webkit-transform":"scale(1.25)"});
+		}, function(){
+			$(this).css({"-webkit-transform":"scale(1)"});	
+	});
+	$("#arrow").click(function() {
+      window.scrollTo(0, 700);
+  	});
+
+  	var num_clicks = 0;
+	$(".hello").click(function() {
+		num_clicks++;
+		if (num_clicks == 1) {
+			$(".hello").html("Welcome! :)");
+			$(".hello").attr("margin-left", "13%");
+			$(".hello").css({"padding-top":"25px"});
+		} else if (num_clicks == 2) {
+			$(".hello").text("print('Hello World!')");
+			$(".hello").css({"margin-left":"13%"});
+			$(".hello").css({"padding-top":"25px"});
+		} else {
+			$(".hello").text("Hello!");
+			num_clicks = 0;
+		}
+	});
 });
-
-
-
-
-
-
-
-
-// 1. show(), fadeIn(), or slideUp()
-// 2. hide(), fadeOut(), or slideDown()
-// 3. css()
-// 4. attr()
-// 5. addClass()
-// 6. removeClass()
-// 7. hasClass()
-// 8. click()
-// 9. hover()
-// 10. text()
-// 11. html()
-// 12. offset()
-// 13. position()
-// 14. scrollTop()
-// 15. append()
-// 16. prepend()
-// 17. children()
-// 18. parent()
